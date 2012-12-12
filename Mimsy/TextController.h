13356a2d6d5dca786bb572b4622f92282f87b855
Jesse Jones <jesse9jones@gmail.com>
2012-12-11 19:53:22 -0800

    Comments

--- Mimsy/TextController.h
+++ Mimsy/TextController.h
@@ -1,5 +1,6 @@
 #import <Cocoa/Cocoa.h>
 
+// Contoller used to mediate between text documents and the NSTextView in the associated window.
 @interface TextController : NSWindowController
 
 @property IBOutlet NSTextView* view;
--- Mimsy/TextDocument.h
+++ Mimsy/TextDocument.h
@@ -1,5 +1,6 @@
 #import <Cocoa/Cocoa.h>
 
+// Document object used when editing text documents.
 @interface TextDocument : NSDocument
 
 - (void)controllerDidLoad;
