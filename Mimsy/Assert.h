dbc9b5a7514bf9ce6ae4e1588852b1c70b12cdf0
Jesse Jones <jesse9jones@gmail.com>
2012-12-30 19:06:09 -0800

    Better comment the differences between the various asserts

--- Mimsy/Assert.h
+++ Mimsy/Assert.h
@@ -1,7 +1,13 @@
-// Unlike assert or NSAssert this version:
+// Unlike both NSAssert and assert this version:
 // 1) Logs failures.
-// 2) Is compiled in in release.
-// 3) Fails fast and hard.
+// 2) Defaults to working in release, but also has a debug only version.
+//
+// Unlike NSAssert this version:
+// 3) Fails fast and hard (NSAsset does all kinds of crazy NSAssertionHandler nonsense).
+// 4) Is easy to set a breakpoint within (by default NSAssert just bails after printing a stack trace).
+//
+// Unlike assert this version:
+// 4) Is easy to set a breakpoint within (although assert, at least, drops you into the debugger when it fires).
 #import "Logger.h"
 
 void _assertFailed(const char* fname, const char* file, int line, const char* expr);
