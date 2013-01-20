98a2cca50ae0f49702e2b3eba3db2629ee6204c9
Jesse Jones <jesse9jones@gmail.com>
2013-01-20 10:09:24 -0800

    Working in ftest infrastructure

---
+++ Mimsy/LuaSupport.h
@@ -0,0 +1,13 @@
+//
+//  LuaSupport.h
+//  Mimsy
+//
+//  Created by Jesse Jones on 1/19/13.
+//  Copyright (c) 2013 Jesse Jones. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LuaSupport : NSObject
+
+@end
---
+++ Mimsy/LuaSupport.m
@@ -0,0 +1,13 @@
+//
+//  LuaSupport.m
+//  Mimsy
+//
+//  Created by Jesse Jones on 1/19/13.
+//  Copyright (c) 2013 Jesse Jones. All rights reserved.
+//
+
+#import "LuaSupport.h"
+
+@implementation LuaSupport
+
+@end
