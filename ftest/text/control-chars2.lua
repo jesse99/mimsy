e1509eaf3bc97bcdc5c62eae9865664321587f01
Jesse Jones <jesse9jones@gmail.com>
2013-01-21 19:00:21 -0800

    Added tests for loading files containing control characters

---
+++ ftest/text/control-chars2.lua
@@ -0,0 +1,25 @@
+-- Verify that Mimsy complains when opening documents with lots of control characters.
+function openedDoc(doc)
+	local data = doc:data()
+	doc:close()
+	
+	-- The document should load complete with control characters (if they are
+	-- undesired users can use Find Gremlins to figure out where they are).
+	local expected = 'The quick brown fox\7jumped over the lazy dog\4\4\4\4\4\4\4\4\4'
+	if data == expected then
+		ftest:expecterror("Found 1 '\\x07' (BELL) and 9 '\\x04' (END OF TRANSMISSION) characters")
+	else
+		ftest:failed(string.format('expected %q but found %q', expected, data))
+	end
+end
+
+function openFailed(reason)
+	ftest:failed(reason)
+end
+
+local fname = '/tmp/control-chars2.txt'
+local file = io.open(fname, 'w')
+file:write('The quick brown fox\7jumped over the lazy dog\4\4\4\4\4\4\4\4\4')
+file:close(file)
+
+app:openfile(fname, 'openedDoc', 'openFailed')
