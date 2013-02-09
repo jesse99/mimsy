920369cce4941c98192fdc27873f611bd286dbcd
Jesse Jones <jesse9jones@gmail.com>
2013-02-08 19:27:28 -0800

    Added setbackcolor, setforecolor, and setfont.
    Also added LUA_ASSERT.

--- Mimsy/LuaSupport.m
+++ Mimsy/LuaSupport.m
@@ -12,6 +12,17 @@
 #import "TextView.h"
 #import "TranscriptController.h"
 
+#define LUA_ASSERT(e, format, ...)						\
+	do													\
+	{													\
+		if (__builtin_expect(!(e), 0))					\
+		{												\
+			__PRAGMA_PUSH_NO_EXTRA_ARG_WARNINGS			\
+			luaL_error(state, format, ##__VA_ARGS__);	\
+			__PRAGMA_POP_NO_EXTRA_ARG_WARNINGS			\
+		}												\
+	} while(0)
+
 static NSColor* textColor(TextDocument* doc, NSString* name)
 {
 	NSColor* color = nil;
@@ -57,6 +68,9 @@ void pushTextDoc(struct lua_State* state, NSDocument* doc)
 		{"getwholeelement", textdoc_getwholeelement},
 		{"resetstyle", textdoc_resetstyle},
 		{"saveas", doc_saveas},
+		{"setbackcolor", textdoc_setbackcolor},
+		{"setfont", textdoc_setfont},
+		{"setforecolor", textdoc_setforecolor},
 		{"setselection", textdoc_setselection},
 		{"setunderline", textdoc_setunderline},
 		{NULL, NULL}
@@ -94,6 +108,8 @@ int app_openfile(struct lua_State* state)
 	const char* path = lua_tostring(state, 2);
 	const char* success = luaL_optstring(state, 3, NULL);
 	const char* failure = luaL_optstring(state, 4, NULL);
+	
+	LUA_ASSERT(path != NULL, "path was nil");
 	
 	NSURL* url = [NSURL fileURLWithPath:[NSString stringWithUTF8String:path]];
 	
@@ -135,6 +151,9 @@ int app_schedule(struct lua_State* state)
 {
 	double secs = lua_tonumber(state, 2);
 	NSString* callback = [NSString stringWithUTF8String:lua_tostring(state, 3)];	// NSString to ensure the memory doesn't go away when we return and pop the stack
+	
+	LUA_ASSERT(secs >= 0.0, "secs was negative");
+	LUA_ASSERT(callback != NULL && callback.length > 0, "callback was NULL or empty");
 	
 	dispatch_queue_t main = dispatch_get_main_queue();
 	dispatch_time_t delay = dispatch_time(DISPATCH_TIME_NOW, (int64_t) (secs*1.0e9));
@@ -185,6 +204,10 @@ int app_addhook(struct lua_State* state)
 {
 	const char* hname = lua_tostring(state, 2);
 	const char* fname = lua_tostring(state, 3);
+	
+	LUA_ASSERT(hname != NULL && strlen(hname) > 0, "hname was NULL or empty");
+	LUA_ASSERT(fname != NULL && strlen(fname) > 0, "fname was NULL or empty");
+
 	[StartupScripts addHook:[NSString stringWithUTF8String:hname] function:[NSString stringWithUTF8String:fname]];
 	return 0;
 }
@@ -194,6 +217,8 @@ int app_addhook(struct lua_State* state)
 int app_log(struct lua_State* state)
 {
 	const char* mesg = lua_tostring(state, 2);
+	LUA_ASSERT(mesg != NULL, "mesg was NULL");
+
 	LOG_INFO("Mimsy", "%s", mesg);
 	return 0;
 }
@@ -203,6 +228,8 @@ int doc_close(struct lua_State* state)
 {
 	lua_getfield(state, 1, "target");
 	NSDocument* doc = (__bridge NSDocument*) lua_touserdata(state, -1);
+	LUA_ASSERT(doc != NULL, "doc was NULL");
+
 	[doc close];		// doesn't ask to save changes
 	return 0;
 }
@@ -217,6 +244,10 @@ int doc_saveas(struct lua_State* state)
 	const char* success = luaL_optstring(state, 4, NULL);
 	const char* failure = luaL_optstring(state, 5, NULL);
 	
+	LUA_ASSERT(doc != NULL, "doc was NULL");
+	LUA_ASSERT(path != NULL && strlen(path) > 0, "path was NULL or empty");
+	LUA_ASSERT(type != NULL && strlen(type) > 0, "type was NULL or empty");
+
 	NSURL* url = [NSURL fileURLWithPath:[NSString stringWithUTF8String:path]];
 	[doc saveToURL:url ofType:[NSString stringWithUTF8String:type] forSaveOperation:NSSaveAsOperation completionHandler:
 		^(NSError* error)
@@ -244,6 +275,7 @@ int doc_saveas(struct lua_State* state)
 int app_stderr(struct lua_State* state)
 {
 	const char* mesg = lua_tostring(state, 2);
+	LUA_ASSERT(mesg != NULL, "mesg was NULL");
 	[TranscriptController writeStderr:[NSString stringWithUTF8String:mesg]];
 	return 0;
 }
@@ -252,6 +284,7 @@ int app_stderr(struct lua_State* state)
 int app_stdout(struct lua_State* state)
 {
 	const char* mesg = lua_tostring(state, 2);
+	LUA_ASSERT(mesg != NULL, "mesg was NULL");
 	[TranscriptController writeStdout:[NSString stringWithUTF8String:mesg]];
 	return 0;
 }
@@ -261,7 +294,8 @@ int textdoc_data(struct lua_State* state)
 {
 	lua_getfield(state, 1, "target");
 	TextDocument* doc = (__bridge TextDocument*) lua_touserdata(state, -1);
-	lua_pushstring(state, doc.controller.text.UTF8String);	
+	LUA_ASSERT(doc != NULL, "doc was NULL");
+	lua_pushstring(state, doc.controller.text.UTF8String);
 	return 1;
 }
 
@@ -272,21 +306,18 @@ int textdoc_getelementat(struct lua_State* state)
 	TextDocument* doc = (__bridge TextDocument*) lua_touserdata(state, -1);
 	lua_Integer loc = lua_tointeger(state, 2) - 1;
 	
+	LUA_ASSERT(doc != NULL, "doc was NULL");
+	LUA_ASSERT(loc >= 0 && loc < doc.controller.text.length, "loc = %d", loc);
+
 	NSString* name = nil;
-	if (loc >= 0 && loc < doc.controller.text.length)
-	{
-		NSTextStorage* storage = doc.controller.textView.textStorage;
-		
-		// Note that effectiveRange is so poorly specified as to be useless.
-		NSDictionary* attrs = [storage attributesAtIndex:(NSUInteger)loc effectiveRange:NULL];
-		if (attrs)
-			name = attrs[@"element name"];
-	}
+	NSTextStorage* storage = doc.controller.textView.textStorage;
 	
-	if (name)
-		lua_pushstring(state, name.UTF8String);
-	else
-		lua_pushnil(state);
+	// Note that effectiveRange is so poorly specified as to be useless.
+	NSDictionary* attrs = [storage attributesAtIndex:(NSUInteger)loc effectiveRange:NULL];
+	if (attrs)
+		name = attrs[@"element name"];
+	
+	lua_pushstring(state, name.UTF8String);
 	
 	return 1;
 }
@@ -297,6 +328,8 @@ int textdoc_getselection(struct lua_State* state)
 	lua_getfield(state, 1, "target");
 	TextDocument* doc = (__bridge TextDocument*) lua_touserdata(state, -1);
 	
+	LUA_ASSERT(doc != NULL, "doc was NULL");
+
 	NSRange selection = doc.controller.textView.selectedRange;
 	lua_pushinteger(state, (lua_Integer) (selection.location+1));
 	lua_pushinteger(state, (lua_Integer) selection.length);
@@ -312,20 +345,20 @@ int textdoc_getwholeelement(struct lua_State* state)
 	NSUInteger loc = (NSUInteger) lua_tointeger(state, 2) - 1;
 	NSUInteger len = (NSUInteger) lua_tointeger(state, 3);
 	
+	LUA_ASSERT(doc != NULL, "doc was NULL");
+	LUA_ASSERT(loc >= 0 && loc+len <= doc.controller.text.length, "loc = %d, len = %d", loc, len);
+
+	NSTextStorage* storage = doc.controller.textView.textStorage;
+	
+	NSRange clipRange;
+	clipRange.location = loc > 0 ? loc-1 : loc;
+	clipRange.length   = MIN(len + 2, doc.controller.text.length - clipRange.location);
+	
 	NSString* name = nil;
-	if (loc < doc.controller.text.length)
-	{
-		NSTextStorage* storage = doc.controller.textView.textStorage;
-		
-		NSRange clipRange;
-		clipRange.location = loc > 0 ? loc-1 : loc;
-		clipRange.length   = MIN(len + 2, doc.controller.text.length - clipRange.location);
-		
-		NSRange effRange;
-		NSDictionary* attrs = [storage attributesAtIndex:loc longestEffectiveRange:&effRange inRange:clipRange];
-		if (attrs && effRange.location == loc && effRange.length == len)
-			name = attrs[@"element name"];
-	}
+	NSRange effRange;
+	NSDictionary* attrs = [storage attributesAtIndex:loc longestEffectiveRange:&effRange inRange:clipRange];
+	if (attrs && effRange.location == loc && effRange.length == len)
+		name = attrs[@"element name"];
 	
 	if (name)
 		lua_pushstring(state, name.UTF8String);
@@ -340,6 +373,7 @@ int textdoc_resetstyle(struct lua_State* state)
 {
 	lua_getfield(state, 1, "target");
 	TextDocument* doc = (__bridge TextDocument*) lua_touserdata(state, -1);
+	LUA_ASSERT(doc != NULL, "doc was NULL");
 	[doc.controller resetStyles];
 	return 0;
 }
@@ -352,15 +386,93 @@ int textdoc_setselection(struct lua_State* state)
 	lua_Integer loc = lua_tointeger(state, 2) - 1;
 	lua_Integer len = lua_tointeger(state, 3);
 	
-	NSTextView* view = doc.controller.textView;
-	luaL_argcheck(state, loc >= 0, 2, "negative");
-	luaL_argcheck(state, loc < view.textStorage.length, 2, "negative");
-	
-	luaL_argcheck(state, len >= 0, 3, "negative");
-	luaL_argcheck(state, loc + len <= view.textStorage.length, 3, "loc+len is oor");
-	
+	LUA_ASSERT(doc != NULL, "doc was NULL");
+	LUA_ASSERT(loc >= 0 && loc+len <= doc.controller.text.length, "loc = %d, len = %d", loc, len);
+
+	NSTextView* view = doc.controller.textView;	
 	[view setSelectedRange:NSMakeRange((NSUInteger)loc, (NSUInteger)len)];
 
+	return 0;
+}
+
+// setbackcolor(textdoc, loc, len, color)
+int textdoc_setbackcolor(struct lua_State* state)
+{
+	lua_getfield(state, 1, "target");
+	TextDocument* doc = (__bridge TextDocument*) lua_touserdata(state, -1);
+	lua_Integer loc = lua_tointeger(state, 2) - 1;
+	lua_Integer len = lua_tointeger(state, 3);
+	const char* cname = lua_tostring(state, 4);
+	
+	LUA_ASSERT(doc != NULL, "doc was NULL");
+	LUA_ASSERT(loc >= 0 && loc+len <= doc.controller.text.length, "loc = %d, len = %d", loc, len);
+	LUA_ASSERT(cname != NULL && strlen(cname) > 0, "color was NULL or empty");
+
+	NSTextStorage* storage = doc.controller.textView.textStorage;
+	NSColor* color = textColor(doc, [NSString stringWithUTF8String:cname]);
+	if (color)
+	{
+		NSRange range = NSMakeRange((NSUInteger)loc, (NSUInteger)len);
+		[storage addAttributes:@{NSBackgroundColorAttributeName: color} range:range];
+	}
+	else
+	{
+		luaL_error(state, "bad color: %s", cname);
+	}		
+	
+	return 0;
+}
+
+// setfont(textdoc, loc, len, name, size)
+int textdoc_setfont(struct lua_State* state)
+{
+	lua_getfield(state, 1, "target");
+	TextDocument* doc = (__bridge TextDocument*) lua_touserdata(state, -1);
+	lua_Integer loc = lua_tointeger(state, 2) - 1;
+	lua_Integer len = lua_tointeger(state, 3);
+	const char* name = lua_tostring(state, 4);
+	lua_Number size = lua_tonumber(state, 5);
+	
+	LUA_ASSERT(doc != NULL, "doc was NULL");
+	LUA_ASSERT(loc >= 0 && loc+len <= doc.controller.text.length, "loc = %d, len = %d", loc, len);
+	LUA_ASSERT(name != NULL && strlen(name) > 0, "name was NULL or empty");
+	LUA_ASSERT(size > 0, "size = %f", (float) size);
+	
+	//LOG_INFO("Mimsy", "font = %s, size = %.1f", name, size);
+	
+	NSTextStorage* storage = doc.controller.textView.textStorage;
+	NSString* s = [NSString stringWithUTF8String:name];
+	NSRange range = NSMakeRange((NSUInteger)loc, (NSUInteger)len);
+	[storage addAttributes:@{NSFontAttributeName: [NSFont fontWithName:s size:size]} range:range];
+	
+	return 0;
+}
+
+// setforecolor(textdoc, loc, len, color)
+int textdoc_setforecolor(struct lua_State* state)
+{
+	lua_getfield(state, 1, "target");
+	TextDocument* doc = (__bridge TextDocument*) lua_touserdata(state, -1);
+	lua_Integer loc = lua_tointeger(state, 2) - 1;
+	lua_Integer len = lua_tointeger(state, 3);
+	const char* cname = lua_tostring(state, 4);
+	
+	LUA_ASSERT(doc != NULL, "doc was NULL");
+	LUA_ASSERT(loc >= 0 && loc+len <= doc.controller.text.length, "loc = %d, len = %d", loc, len);
+	LUA_ASSERT(cname != NULL && strlen(cname) > 0, "color was NULL or empty");
+	
+	NSTextStorage* storage = doc.controller.textView.textStorage;
+	NSColor* color = textColor(doc, [NSString stringWithUTF8String:cname]);
+	if (color)
+	{
+		NSRange range = NSMakeRange((NSUInteger)loc, (NSUInteger)len);
+		[storage addAttributes:@{NSForegroundColorAttributeName: color} range:range];
+	}
+	else
+	{
+		luaL_error(state, "bad color: %s", cname);
+	}
+	
 	return 0;
 }
 
@@ -375,57 +487,53 @@ int textdoc_setunderline(struct lua_State* state)
 	const char* pattern = luaL_optstring(state, 5, "solid");
 	const char* cname = luaL_optstring(state, 6, NULL);
 	
+	LUA_ASSERT(doc != NULL, "doc was NULL");
+	LUA_ASSERT(loc >= 0 && loc+len <= doc.controller.text.length, "loc = %d, len = %d", loc, len);
+	
 	NSTextStorage* storage = doc.controller.textView.textStorage;
-	if (loc >= 0 && len >= 0 && loc + len <= storage.length)
-	{
-		NSMutableDictionary* attrs = [NSMutableDictionary new];
-		
-		int value = 0;
-		if (strcmp(style, "none") == 0)			// TODO: may also want to support NSUnderlineByWordMask
-			value |= NSUnderlineStyleNone;
-		else if (strcmp(style, "single") == 0)
-			value |= NSUnderlineStyleSingle;
-		else if (strcmp(style, "thick") == 0)
-			value |= NSUnderlineStyleThick;
-		else if (strcmp(style, "double") == 0)
-			value |= NSUnderlineStyleDouble;
-		else
-			luaL_error(state, "bad style: %s", style);
-
-		if (strcmp(pattern, "solid") == 0)
-			value |= NSUnderlinePatternSolid;
-		else if (strcmp(pattern, "dot") == 0)
-			value |= NSUnderlinePatternDot;
-		else if (strcmp(pattern, "dash") == 0)
-			value |= NSUnderlinePatternDash;
-		else if (strcmp(pattern, "dashdot") == 0)
-			value |= NSUnderlinePatternDashDot;
-		else if (strcmp(pattern, "dashdotdot") == 0)
-			value |= NSUnderlinePatternDashDotDot;
-		else
-			luaL_error(state, "bad pattern: %s", pattern);
-		attrs[NSUnderlineStyleAttributeName] = [NSNumber numberWithInt:value];
-		
-		if (cname)
-		{
-			NSColor* color = textColor(doc, [NSString stringWithUTF8String:cname]);
-			if (color)
-			{
-				attrs[NSUnderlineColorAttributeName] = color;
-			}
-			else
-			{
-				luaL_error(state, "bad color: %s", cname);
-			}
-		}
-		
-		NSRange range = NSMakeRange((NSUInteger)loc, (NSUInteger)len);
-		[storage addAttributes:attrs range:range];
-	}
+	NSMutableDictionary* attrs = [NSMutableDictionary new];
+	
+	int value = 0;
+	if (strcmp(style, "none") == 0)			// TODO: may also want to support NSUnderlineByWordMask
+		value |= NSUnderlineStyleNone;
+	else if (strcmp(style, "single") == 0)
+		value |= NSUnderlineStyleSingle;
+	else if (strcmp(style, "thick") == 0)
+		value |= NSUnderlineStyleThick;
+	else if (strcmp(style, "double") == 0)
+		value |= NSUnderlineStyleDouble;
 	else
+		luaL_error(state, "bad style: %s", style);
+
+	if (strcmp(pattern, "solid") == 0)
+		value |= NSUnderlinePatternSolid;
+	else if (strcmp(pattern, "dot") == 0)
+		value |= NSUnderlinePatternDot;
+	else if (strcmp(pattern, "dash") == 0)
+		value |= NSUnderlinePatternDash;
+	else if (strcmp(pattern, "dashdot") == 0)
+		value |= NSUnderlinePatternDashDot;
+	else if (strcmp(pattern, "dashdotdot") == 0)
+		value |= NSUnderlinePatternDashDotDot;
+	else
+		luaL_error(state, "bad pattern: %s", pattern);
+	attrs[NSUnderlineStyleAttributeName] = [NSNumber numberWithInt:value];
+	
+	if (cname)
 	{
-		luaL_error(state, "bad range: loc=%d, len=%d", loc, len);
+		NSColor* color = textColor(doc, [NSString stringWithUTF8String:cname]);
+		if (color)
+		{
+			attrs[NSUnderlineColorAttributeName] = color;
+		}
+		else
+		{
+			luaL_error(state, "bad color: %s", cname);
+		}
 	}
+	
+	NSRange range = NSMakeRange((NSUInteger)loc, (NSUInteger)len);
+	[storage addAttributes:attrs range:range];
 	
 	return 0;
 }
