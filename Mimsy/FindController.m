a0f8cbf021cfa5545d3fd4926d74bab2ae6be452
Jesse Jones <jesse9jones@gmail.com>
2014-07-26 16:01:16 -0700

    Fixed a few asserts.

--- Mimsy/FindController.m
+++ Mimsy/FindController.m
@@ -104,7 +104,7 @@ typedef void (^FindBlock)(BaseTextController* controller, NSRegularExpression* r
 					   }
 				   };
 			   
-			   ASSERT(searchRange.location < _text.length);
+			   ASSERT(searchRange.location <= _text.length);
 			   ASSERT(searchRange.location + searchRange.length <= _text.length);
 			   [regex enumerateMatchesInString:_text options:options range:searchRange usingBlock:matcher];
 			   
@@ -114,7 +114,7 @@ typedef void (^FindBlock)(BaseTextController* controller, NSRegularExpression* r
 				   {
 					   searchRange = NSMakeRange(0, searchFrom - 1);
 
-					   ASSERT(searchRange.location < _text.length);
+					   ASSERT(searchRange.location <= _text.length);
 					   ASSERT(searchRange.location + searchRange.length <= _text.length);
 					   [regex enumerateMatchesInString:_text options:options range:searchRange usingBlock:matcher];
 				   }
