4b8379a14bb368eb1805a9d43f113039d115572e
Jesse Jones <jesse9jones@gmail.com>
2015-06-14 13:46:43 -0700

    Comment

--- Mimsy/GlyphsAttribute.h
+++ Mimsy/GlyphsAttribute.h
@@ -5,6 +5,7 @@ extern NSString* GlyphsAttributeName;
 // Contains a sequence of glyphs used instead of the normal glyphs for a range of text.
 @interface GlyphsAttribute : NSObject
 
+// See extensions-Proc Files for an explanation of these arguments.
 - (id)initWithStyle:(NSDictionary*)style chars:(NSString*)chars repeat:(bool)repeat;
 
 - (NSUInteger)numGlyphs;
