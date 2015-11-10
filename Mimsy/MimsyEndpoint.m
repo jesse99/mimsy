84de62d1fbf46043860a374efdfde0dea407ec92
Jesse Jones <jesse9jones@gmail.com>
2015-11-09 20:12:45 -0800

    Switched to using native sockets

--- Mimsy/MimsyEndpoint.m
+++ Mimsy/MimsyEndpoint.m
@@ -1,6 +1,7 @@
 #import "MimsyEndpoint.h"
 
 #import <CoreFoundation/CoreFoundation.h>
+#include <fcntl.h>
 #include <sys/fcntl.h>
 #include <sys/socket.h>
 #include <netinet/tcp.h>
@@ -14,7 +15,7 @@ static CFSocketRef _acceptSocket;
 // -----------------------------------------------------------------------------------------------------------
 @interface ExtensionConnection : NSObject
 
-- (id)initWithInputStream:(NSInputStream*)inputStream outputStream:(NSOutputStream*)outputStream;
+- (id)init:(CFSocketNativeHandle)socketH;
 
 - (void)open;
 - (void)close;
@@ -31,22 +32,20 @@ static NSMutableArray* _extensions;
 
 @implementation ExtensionConnection
 {
-    NSInputStream* _inputStream;
-    NSOutputStream* _outputStream;
-    bool _needsReply;
-    bool _canReply;
+    CFSocketNativeHandle _socket;
 }
 
-- (id)initWithInputStream:(NSInputStream*)inputStream outputStream:(NSOutputStream*)outputStream
+- (id)init:(CFSocketNativeHandle)socketH
 {
+    ASSERT(socketH >= 0);
+    
     if (!_extensions)
         _extensions = [NSMutableArray new];
     
     self = [super init];
     if (self != nil)
     {
-        _inputStream = inputStream;
-        _outputStream = outputStream;
+        _socket = socketH;
         _name = @"";
     }
     return self;
@@ -54,91 +53,62 @@ static NSMutableArray* _extensions;
 
 - (void)open
 {
-    LOG("Extensions", "Opened connection to extension");
-    [_inputStream  setDelegate:self];
-    [_outputStream setDelegate:self];
-    [_inputStream  scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-    [_outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-    [_inputStream  open];
-    [_outputStream open];
+    LOG("Extensions", "Opening connection to extension");
     [_extensions addObject:self];
+    
+    NSString* message = [self _readMessageWithTimeout];
+    if (message)
+    {
+        [self _writeMessage:@"OK"];
+    }
 }
 
 - (void)close
 {
-    [_inputStream  setDelegate:nil];
-    [_outputStream setDelegate:nil];
-    [_inputStream  close];
-    [_outputStream close];
-    [_inputStream  removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-    [_outputStream removeFromRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
-    LOG("Extensions", "Closed connection to %s extension", STR(_name));
-    [_extensions removeObject:self];
+    if (_socket >= 0)
+    {
+        (void) close(_socket);
+        _socket = -1;
+        [_extensions removeObject:self];
+        LOG("Extensions", "Closed connection to %s extension", STR(_name));
+    }
 }
 
-- (void)stream:(NSStream*)stream handleEvent:(NSStreamEvent)event
+- (NSString*)_readMessageWithTimeout
 {
-    if (event == NSStreamEventEndEncountered)
-    {
-        LOG("Extensions", "EOF for %s extension connection", STR(_name));
-        [self close];
-    }
-    else if (event == NSStreamEventErrorOccurred)
-    {
-        NSString* reason = [[_outputStream streamError] localizedFailureReason];
-        LOG("Error", "Error for %s extension connection: %s", STR(_name), STR(reason));
-        [self close];
-    }
-    else if (stream == _inputStream && event == NSStreamEventHasBytesAvailable)
-    {
-        NSString* message = [self _read];
-        if (message)
-        {
-            if (_canReply)
-            {
-                const char* reply = "OK\n";
-                [self _write:reply bytes:strlen(reply)];
-            }
-            else
-            {
-                _needsReply = true;
-            }
-        }
-    }
-    else if (stream == _outputStream && event == NSStreamEventHasSpaceAvailable)
-    {
-        // This is pretty dorky: we're using loopback so you'd think we could just send replies without
-        // worrying about blocking (especially if we make the socket non-blocking). But this doesn't
-        // work, probably because NSOutputStream is trying to be helpful. To make matters worse we
-        // only get the NSStreamEventHasSpaceAvailable once which means we need that lame _canReply
-        // flag.
-        if (_needsReply)
-        {
-            const char* reply = "OK\n";
-            [self _write:reply bytes:strlen(reply)];
+    double startTime = getTime();
 
-            _needsReply = false;
-        }
-        else
-        {
-            _canReply = true;
-        }
+    while (_socket >= 0)
+    {
+        double currentTime = getTime();
+        if (currentTime - startTime > 1.0)
+            break;
+        
+        if ([self _messageAvailable])
+            return [self _readMessage];
+        
+        usleep(100*1000);
     }
+    
+    LOG("Error", "Timed out reading message from %s extension connection", STR(_name));
+    [self close];
+
+    return nil;
 }
 
-- (NSString*)_read
+- (NSString*)_readMessage
 {
     NSString* message = nil;
     
     uint32_t bytesToRead;
-    if ([self _primitiveRead:&bytesToRead bytes:sizeof(bytesToRead)])
+    if ([self _read:&bytesToRead bytes:sizeof(bytesToRead)])
     {
         bytesToRead = ntohl(bytesToRead);
         
         char* payload = malloc(bytesToRead+1);
         if (payload)
         {
-            if ([self _primitiveRead:payload bytes:bytesToRead])
+            if ([self _read:payload bytes:bytesToRead])
             {
                 payload[bytesToRead] = '\0';
                 
@@ -151,6 +121,7 @@ static NSMutableArray* _extensions;
             else
             {
                 LOG("Error", "Failed to read %d byte payload for %s extension connection", bytesToRead, STR(_name));
+                [self close];
             }
             free(payload);
         }
@@ -163,50 +134,93 @@ static NSMutableArray* _extensions;
     else
     {
         LOG("Error", "Failed to read payload size for %s extension connection", STR(_name));
+        [self close];
     }
     
     return message;
 }
 
-- (bool)_write:(const void*)buffer bytes:(NSUInteger)bytes
+- (bool)_messageAvailable
 {
-    uint32_t bytesToWrite = htonl(bytes);
-    return [self _primitiveWrite:&bytesToWrite bytes:sizeof(bytesToWrite)] && [self _primitiveWrite:buffer bytes:bytes];
+    while (_socket >= 0)
+    {
+        char buffer[4];
+        ssize_t read = recv(_socket, buffer, sizeof(buffer), MSG_PEEK);
+        if (read == sizeof(buffer))
+        {
+            return true;
+        }
+        else if (read >= 0)
+        {
+            return false;
+        }
+        else if (errno != EINTR)
+        {
+            LOG("Error", "Failed to peek message for %s extension connection: %s", STR(_name), strerror(errno));
+            [self close];
+        }
+    }
+    
+    return false;
 }
 
-- (bool)_primitiveRead:(void*)buffer bytes:(NSUInteger)bytes
+- (bool)_read:(void*)buffer bytes:(NSUInteger)bytes
 {
-    NSInteger bytesRead = 0;
+    ssize_t bytesRead = 0;
     
-    while (_inputStream.hasBytesAvailable && bytesRead < bytes)
+    while (bytesRead < bytes && _socket >= 0)
     {
-        // If there was an error we'll wait for NSStreamEventEndEncountered or NSStreamEventErrorOccurred
-        // to come around.
-        bytesRead += [_inputStream read:(uint8_t*) (buffer + bytesRead) maxLength:bytes - (NSUInteger) bytesRead];
+        ssize_t read = recv(_socket, buffer + bytesRead, bytes - (size_t) bytesRead, 0);
+        if (read < 0)
+        {
+            if (errno != EINTR)
+            {
+                LOG("Error", "Failed to read %lu bytes for %s extension connection: %s", (unsigned long)bytes, STR(_name), strerror(errno));
+                [self close];
+            }
+        }
+        else
+        {
+            bytesRead += read;
+        }
     }
     
     return bytesRead == bytes;
 }
 
-- (bool)_primitiveWrite:(const void*)buffer bytes:(NSUInteger)bytes
+- (bool)_writeMessage:(NSString*)message
 {
-    NSInteger bytesWritten = [_outputStream write:buffer maxLength:bytes];
-    if (bytesWritten < 0)
+    const char* data = message.UTF8String;
+    unsigned long len = strlen(data);
+    
+    uint32_t bytesToWrite = htonl(len);
+    return [self _write:&bytesToWrite bytes:sizeof(bytesToWrite)] && [self _write:data bytes:len];
+}
+
+- (bool)_write:(const void*)buffer bytes:(NSUInteger)bytes
+{
+    ssize_t bytesSent = 0;
+    
+    while (bytesSent < bytes && _socket >= 0)
     {
-        NSString* reason = [[_outputStream streamError] localizedFailureReason];
-        LOG("Error", "There was an error sending to the %s extension: %s", STR(_name), STR(reason));
-        [self close];
-    }
-    else if (bytesWritten < bytes)
-    {
-        // Should only happen if the extension stops reading data and the send buffer
-        // fills up.
-        LOG("Error", "Sent %ld instead of %lu bytes to %s extension", (long)bytesWritten, (unsigned long)bytes, STR(_name));
-        [self close];
+        ssize_t written = send(_socket, buffer + bytesSent, bytes - (size_t) bytesSent, 0);
+        if (written < 0)
+        {
+            if (errno != EINTR)
+            {
+                LOG("Error", "Failed to send %lu bytes for %s extension connection: %s", (unsigned long)bytes, STR(_name), strerror(errno));
+                [self close];
+            }
+        }
+        else
+        {
+            bytesSent += written;
+        }
     }
     
-    return bytesWritten == bytes;
+    return bytesSent == bytes;
 }
+
 @end
 
 // -----------------------------------------------------------------------------------------------------------
@@ -219,26 +233,8 @@ static void acceptCallback(CFSocketRef socket, CFSocketCallBackType callbackType
     const int yes = 1;
     (void) setsockopt(socketH, IPPROTO_TCP, TCP_NODELAY, &yes, sizeof(yes));
     
-    CFReadStreamRef readStream = NULL;
-    CFWriteStreamRef writeStream = NULL;
-    CFStreamCreatePairWithSocket(kCFAllocatorDefault, socketH, &readStream, &writeStream);
-    if (readStream && writeStream)
-    {
-        CFReadStreamSetProperty(readStream, kCFStreamPropertyShouldCloseNativeSocket, kCFBooleanTrue);
-        CFWriteStreamSetProperty(writeStream, kCFStreamPropertyShouldCloseNativeSocket, kCFBooleanTrue);
-        
-        ExtensionConnection* connection = [[ExtensionConnection alloc] initWithInputStream:(__bridge NSInputStream*)(readStream) outputStream:(__bridge NSOutputStream*)(writeStream)];
-        [connection open];
-    }
-    else
-    {
-        // On any failure, we need to destroy the CFSocketNativeHandle
-        // since we are not going to use it any more.
-        (void) close(socketH);
-    }
-
-    if (readStream) CFRelease(readStream);
-    if (writeStream) CFRelease(writeStream);
+    ExtensionConnection* connection = [[ExtensionConnection alloc] init:socketH];
+    [connection open];
 }
 
 @implementation MimsyEndpoint
