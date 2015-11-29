2fad43e3f07395ef9f664baa962538872a67ceec
Jesse Jones <jesse9jones@gmail.com>
2015-11-28 23:48:46 -0800

    Use exception handling

--- GoFormat/Plugin.swift
+++ GoFormat/Plugin.swift
@@ -1,6 +1,25 @@
 import Cocoa
 import MimsyPlugins
 
+enum TaskError: ErrorType
+{
+    case GenericError(text: String)
+    case ProcessError(status: Int32, stdout: String, stderr: String)
+}
+
+func readToEOF(name: String, _ file: NSFileHandle) throws -> String
+{
+    let data = file.readDataToEndOfFile()   // zero bytes is not an error (e.g. for stderr)
+    if let result = NSString(data: data, encoding: NSUTF8StringEncoding)
+    {
+        return result as String
+    }
+    else
+    {
+        throw TaskError.GenericError(text: "failed to convert \(name) to UTF8")
+    }
+}
+
 class StdGoFormat: MimsyPlugin
 {
     override func onLoad(stage: Int) -> String?
@@ -31,38 +50,44 @@ class StdGoFormat: MimsyPlugin
                 task.standardInput = stdinP
                 
                 let data = view.text.dataUsingEncoding(NSUTF8StringEncoding)
-                stdinP.fileHandleForWriting.writeData(data!)
+                stdinP.fileHandleForWriting.writeData(data!)    // should always be able to convert to UTF8
                 stdinP.fileHandleForWriting.closeFile()
                 
-                let stdout = stdoutP.fileHandleForReading
-                let stderr = stderrP.fileHandleForReading
                 task.launch()
                 
-                if let result = NSString(data: stdout.readDataToEndOfFile(), encoding: NSUTF8StringEncoding)
+                do
                 {
-                    if task.terminationStatus == 0
+                    let stdout = try readToEOF("stdout", stdoutP.fileHandleForReading)
+                    if task.terminationStatus != 0
                     {
-                        view.setText(result as String, undoText: "Format")
+                        let stderr = try readToEOF("stderr", stderrP.fileHandleForReading)
+                        throw TaskError.ProcessError(status: task.terminationStatus, stdout: stdout, stderr: stderr)
                     }
-                    else
+
+                    view.setText(stdout, undoText: "Format")
+                }
+                catch TaskError.GenericError(let text)
+                {
+                    log("Error", "error running gofmt: %@", text)
+                }
+                catch TaskError.ProcessError(let status, let stdout, let stderr)
+                {
+                    log("Error", "gofmt exited with code \(status)")
+                    
+                    if !stdout.isEmpty
                     {
-                        log("Error", "gofmt exited with code \(task.terminationStatus)")
-                        
-                        if result.length > 0
-                        {
-                            log("Error", "stdout: %@", result)
-                        }
-                        
-                        if let err = NSString(data: stderr.readDataToEndOfFile(), encoding: NSUTF8StringEncoding)
-                        {
-                            if err.length > 0
-                            {
-                                log("Error", "stderr: %@", err)
-                            }
-                        }
+                        log("Error", "stdout: %@", stdout)
+                    }
+                    
+                    if !stderr.isEmpty
+                    {
+                        log("Error", "stderr: %@", stderr)
                     }
                 }
-                // TODO: handle encoding failures better
+                catch
+                {
+                    log("Error", "unknown gofmt error")
+                }
             }
         }
     }
